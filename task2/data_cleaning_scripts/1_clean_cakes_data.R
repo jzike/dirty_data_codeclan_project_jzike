ce765a415cb1b0235546f8b6f2f0407aa7fb0cc7
jzike <107878458+jzike@users.noreply.github.com>
2022-08-01 12:36:46 +0100

    Update 1_clean_cakes_data.R

--- task2/data_cleaning_scripts/1_clean_cakes_data.R
+++ task2/data_cleaning_scripts/1_clean_cakes_data.R
@@ -1,11 +1,13 @@
-# Libraries
+# 1. Libraries ----
+
 library(tidyverse)
 
-# Read in data
+# 2. Read in data ----
+
 cake_ingredients <- read_csv("raw_data/cake-ingredients-1961.csv")
 cake_codes <- read_csv("raw_data/cake_ingredient_code.csv")
  
-
+# 3. Tidy data ----
 # Pivot data longer - move ingredients to a column and amounts to a separate column
 
 cake_ingredients_long <- cake_ingredients %>% 
@@ -21,7 +23,7 @@ cakes_joined <- cake_ingredients_long %>%
   left_join(cake_codes, by = c("ingredient_code" = "code")) %>% 
   janitor::clean_names()
 
-# Find and deal with missing values
+# 4. Find and deal with missing values ----
 
 ## Check for missing values in measure
 cakes_joined %>% 
@@ -30,6 +32,14 @@ cakes_joined %>%
 
 ## Drop rows where amount = NA, NA in amount means the ingredient isn't in that cake
 
-
+cakes_joined <- cakes_joined  %>% 
+  filter(!is.na(amount))
 
 ## Change NAs to "cup" in measure, only ingredients called "Sour cream cup" are missing an amount, meaning that the measure has been mistakenly added to the ingredient column
+
+cakes_imputed <- cakes_joined %>% 
+  mutate(measure = coalesce(measure, "cup"))
+
+# 5. Write clean data csv ----
+
+write_csv(cakes_imputed, "clean_data/tidy_cakes.csv")
